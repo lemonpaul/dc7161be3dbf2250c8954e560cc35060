c929735d4b5ffaf06221176a1eceb32bf098ee42
lemonpaul <leamonpaul@yandex.ru>
2020-01-25 02:08:01 +0300

    #5 Use manual add form fields check

--- functions/models.py
+++ functions/models.py
@@ -9,9 +9,9 @@ from django.core import validators
 from django.contrib.postgres.fields import ArrayField
 
 class Function(models.Model):
-    formula = models.CharField(max_length=200, verbose_name="Функция")
-    interval = models.IntegerField(default=0, validators=[validators.MinValueValidator(1, message="Интервал должен натуральным числом.")], verbose_name="Интервал (дни)")
-    step = models.IntegerField(default=0, validators=[validators.MinValueValidator(1, message="Шаг должен быть натуральным числом.")], verbose_name="Шаг (часы)")
+    formula = models.CharField(max_length=200)
+    interval = models.IntegerField(default=0)
+    step = models.IntegerField(default=0)
     modified = models.DateTimeField(auto_now_add=True)
     marks=ArrayField(models.IntegerField(), null=True, blank=True)
     values = ArrayField(models.FloatField(), null=True, blank=True)
