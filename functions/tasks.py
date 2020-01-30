a4159e71b656a5ef5ff258e6c189b20d46b622fe
lemonpaul <leamonpaul@yandex.ru>
2020-01-30 12:28:34 +0300

    #21 Use map for generate data instead cycle

--- functions/tasks.py
+++ functions/tasks.py
@@ -10,22 +10,17 @@ from celery import shared_task
 
 from .models import Function
 
+
 @shared_task
 def generate_data(function_id):
     function = Function.objects.get(pk=function_id)
     start = function.modified - datetime.timedelta(days=function.interval)
     finish = function.modified
-    marks = list()
-    values = list()
     try:
-        while start <= finish:
-            t = round(start.timestamp())
-            f = eval(function.formula)
-            marks.append(t)
-            values.append(f)
-            start += datetime.timedelta(hours=function.step)
-        data = {'infile': {'title': {'text': ''}, 'xAxis': {'categories': marks}, 'yAxis': {'title': {'text': ''}},
-                           'series': [{'showInLegend': False, 'name': 'y(t)', 'data': values}]}}
+        t = list(range(round(start.timestamp()), round(finish.timestamp())+1, function.step * 3600))
+        f = list(map(lambda t: eval(function.formula), t))
+        data = {'infile': {'title': {'text': ''}, 'xAxis': {'categories': t}, 'yAxis': {'title': {'text': ''}},
+                           'series': [{'showInLegend': False, 'name': 'f(t)', 'data': f}]}}
         http = urllib3.PoolManager()
         response = http.request('POST', 'http://highcharts:8080', headers={'Content-Type': 'application/json'}, body=json.dumps(data))
         open('media/images/plot.png', 'wb').write(response.data)
