079a6d021e41c618805c7f964c8a3c326a75d664
lemonpaul <leamonpaul@yandex.ru>
2020-02-06 15:53:16 +0300

    #31 Remove function application

--- dashboard/wsgi.py
+++ dashboard/wsgi.py
@@ -3,7 +3,7 @@ WSGI config for dashboard project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
-For more information on this file, see
+For more information on this .gitignore, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
