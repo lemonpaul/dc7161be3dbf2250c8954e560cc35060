80698157170b32b6877c6c3fb1648caaa080c3e6
lemonpaul <leamonpaul@yandex.ru>
2020-02-13 15:22:20 +0300

    #36 Refactor generate_data function

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -3,7 +3,8 @@ import urllib3
 import json
 import os
 
-from math import *
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
 
 from django.core.files import File
 from celery import shared_task
@@ -13,26 +14,25 @@ from .models import Function
 
 @shared_task
 def generate_data(function_id):
-    function = Function.objects.get(pk=function_id)
-    start = function.modified - datetime.timedelta(days=function.interval)
-    finish = function.modified
+    func = Function.objects.get(pk=function_id)
+    start = func.modified - datetime.timedelta(days=func.interval)
+    finish = func.modified
+    t = list(range(round(start.timestamp()), round(finish.timestamp()) + 1, func.step * 3600))
     try:
-        t = list(range(round(start.timestamp()), round(finish.timestamp())+1, function.step * 3600))
-        f = list(map(lambda t: eval(function.formula), t))
-        data = {'infile': {'title': {'text': ''}, 'xAxis': {'categories': t}, 'yAxis': {'title': {'text': ''}},
-                           'series': [{'showInLegend': False, 'name': 'f(t)', 'data': f}]}}
-        http = urllib3.PoolManager()
-        response = http.request('POST', 'http://highcharts:8080', headers={'Content-Type': 'application/json'},
-                                body=json.dumps(data))
-        open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                          'media/images/plot.png'), 'wb').write(response.data)
-        if function.plot:
-            if os.path.isfile(function.plot.path):
-                os.remove(function.plot.path)
-        function.plot.save('plot.png', File(open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                                              'media/images/plot.png'), 'rb')))
-        os.remove(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'media/images/plot.png'))
-    except (NameError, ValueError, SyntaxError) as error:
-        function.error = error
-    function.save()
+        f = list(map(lambda x: float(parse_expr(func.formula).subs({"t": float(x)})), t))
+    except TypeError as error:
+        func.error = error
+        func.save()
+        return
+    data = {'infile': {'title': {'text': ''}, 'xAxis': {'categories': t}, 'yAxis': {'title': {'text': ''}},
+                       'series': [{'showInLegend': False, 'name': 'f(t)', 'data': f}]}}
+    http = urllib3.PoolManager()
+    response = http.request('POST', 'http://highcharts:8080', headers={'Content-Type': 'application/json'},
+                            body=json.dumps(data))
+    if func.plot:
+        os.remove(func.plot.path)
+    open('plot.png', 'wb').write(response.data)
+    func.plot.save('%s.png' % datetime.datetime.now().timestamp(), File(open('plot.png', 'rb')))
+    os.remove('plot.png')
+    func.save()
     return
